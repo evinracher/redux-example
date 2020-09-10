4ae1a63f32f945093de2f4601f409ad985837a40
evinracher <eevinley@gmail.com>
2020-09-10 15:05:00 -0500

    Adding examples

--- src/App.js
+++ src/App.js
@@ -1,26 +1,27 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import { Provider } from 'react-redux'
+import store from './redux/store'
 
-function App() {
+import Counter from './components/Counter'
+// import Info from './components/Info'
+// import Fruits from './components/Fruits'
+// import Todos from './components/Todos'
+// import Controller from './components/Controller'
+// import Blog from './components/Blog'
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <Provider store={store}>
+      <div>
+        {/* <Info /> */}
+        <Counter />
+        {/* <Fruits /> */}
+        {/* <Todos /> */}
+        {/* <Controller /> */}
+        {/* <Blog /> */}
+      </div>
+    </Provider>
+  )
 }
 
-export default App;
+export default App
